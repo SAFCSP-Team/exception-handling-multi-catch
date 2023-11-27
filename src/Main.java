3b8436f2ebdee25928a5b8884f2d5723059c5bdb
nourabyte <148945652+nourabyte@users.noreply.github.com>
2023-11-27 20:45:02 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] array = {10, 20, 30};
+
+        int zero = 0;
+     
+            System.out.println("before divide");
+            int result = (array[2] / zero);
+            System.out.println("after divide");
+             }
+}
